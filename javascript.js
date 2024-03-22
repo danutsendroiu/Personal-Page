da24582a258dc545489ca3ba90ef1879ceb2d252
Danut Sendroiu <danut.sendroiu@gmail.com>
2024-03-22 22:30:05 +0200

    javaScript added

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var question = prompt("Are you reddy to see my first attempt to make a personal page?")
+var answer = ["yes" , "no"];
+
+
+if (question === answer[0]){
+    alert ("Thank you for your confidence!");
+}else if(question === answer[1]){
+    alert("I really think I deserve a little trust!")
+}
+else{
+    alert("The page will load anyway! :D ")
+}
